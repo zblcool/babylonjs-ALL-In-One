403edaf59ca6a152efc45f2459c3a3a3d823cd2d
Ash <9151815+zblcool@users.noreply.github.com>
2023-08-27 23:55:06 +1000

    feat(movement): pick the box to move around

--- all-in-one/src/App.tsx
+++ all-in-one/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Scene } from "@babylonjs/core";
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Scene, PointerDragBehavior } from "@babylonjs/core";
 import SceneComponent from "./SceneComponent"; // uses above component in same directory
 // import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
 import { Inspector } from '@babylonjs/inspector';
 
 let box: any; // Define the box object which will be able to be accessed by other functions
-
+let ground : any;
 
 const onSceneReady = (scene : Scene) => {
 
@@ -30,12 +30,36 @@ const onSceneReady = (scene : Scene) => {
 
   // Our built-in 'box' shape.
   box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+ // Our built-in 'ground' shape.
+ ground = MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 
+  var pointerDragBehavior = new PointerDragBehavior({dragPlaneNormal: new Vector3(0,1,0)});
+    
+  // Use drag plane in world space
+  pointerDragBehavior.useObjectOrientationForDragging = false;
+
+  // Listen to drag events
+  pointerDragBehavior.onDragStartObservable.add((event)=>{
+      console.log("dragStart");
+      console.log(event);
+  })
+  pointerDragBehavior.onDragObservable.add((event)=>{
+      console.log("drag");
+      console.log(event);
+  })
+  pointerDragBehavior.onDragEndObservable.add((event)=>{
+      console.log("dragEnd");
+      console.log(event);
+  })
+
+  // If handling drag events manually is desired, set move attached to false
+  // pointerDragBehavior.moveAttached = false;
+
+  box.addBehavior(pointerDragBehavior);
   // Move the box upward 1/2 its height
   box.position.y = 1;
 
-  // Our built-in 'ground' shape.
-  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+ 
 };
 
 
